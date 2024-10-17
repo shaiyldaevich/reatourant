1946fc59240b766074dadb024b300925f5e97ecf
nurmuhammetjs13 <nurmuhammedaltybaev@gmail.com>
2024-10-17 22:08:48 +0600

    added images for aboutus and bestseller

--- src/components/pages/AboutUs/AboutUs.tsx
+++ src/components/pages/AboutUs/AboutUs.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import scss from "./AboutUs.module.scss";
 
 const AboutUs = () => {
-    return <div>AboutUs</div>;
+    return (
+        <section className={scss.AboutUs}>
+            <div className={scss.container}>
+                <div className={scss.content}></div>
+            </div>
+        </section>
+    );
 };
 
 export default AboutUs;
