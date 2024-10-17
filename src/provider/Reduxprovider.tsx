edf2a6a8c39ac96705bb6a05b21cbe5463592db6
nurmuhammetjs13 <nurmuhammedaltybaev@gmail.com>
2024-10-17 15:13:37 +0600

    aadd

--- src/provider/Reduxprovider.tsx
+++ src/provider/Reduxprovider.tsx
@@ -4,11 +4,11 @@ import { FC, ReactNode } from "react";
 import { Provider } from "react-redux";
 
 interface ReduxProviderProps {
-  children: ReactNode;
+    children: ReactNode;
 }
 
 const ReduxProvider: FC<ReduxProviderProps> = ({ children }) => {
-  return <Provider store={store}>{children}</Provider>;
+    return <Provider store={store}>{children}</Provider>;
 };
 
 export default ReduxProvider;
