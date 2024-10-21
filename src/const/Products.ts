f5c3b3d42b8adcf8f5332e41a5ea65236c2b0c33
Aziret055 <kojom5erd1ev1@gmail.com>
2024-10-21 14:30:31 +0600

    finshedmenu

---
+++ src/const/Products.ts
@@ -0,0 +1,203 @@
+export const products = [
+	// Desserts
+	{
+		id: 'dess-1a7f-98c3-dc5b-icecreammaster',
+		name: 'Ice Cream Master',
+		price: 9.11,
+		imageSrc:
+			'https://s3-alpha-sig.figma.com/img/d524/3b1a/d0d54568341045c1e0f2b7d576ea4793?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nVr84UbRTsPhUK3wpTUc-FfKEpCOp8SKNCtRgbN~5Nn0aYnJ6kQXfd5xWDtUYXvlJ8KLslcRNjXNOpP7A980aE6QqClo0tUnnt~BN4RKG~8g7Op1ZsW1kINyGXh7BxMrhuad-kPwlGpgGqM1NcB4nmWVBHzMDRK3O4qVQPz5xFxOipnPpAeVMhCqpEYKRp7PM5q5CjMw7s-U4AXRLApGQLy8Zbos6cushSSdMlxSD4dQSTebdBkH3tDkevBNnoSj4z~NS72R5t3rZdneht0rTJ3bpmtOfou1esCAD~38TzymGOCSl~uGlLQyRUZB~B8Hcr0wWxizMoy9FHbcgdJY-Q__',
+		ingredients: ['soda', 'cream', 'milk', 'sugar'],
+		categoryId: 'desserts-001',
+		description:
+			'A delightful mix of soda and creamy ice cream topped with your favorite flavors.'
+	},
+	{
+		id: 'dess-2b3d-42e9-a1f8-icecreamhelp',
+		name: 'Ice Cream Help',
+		price: 10.5,
+		imageSrc:
+			'https://s3-alpha-sig.figma.com/img/e8da/4c3a/b44f0f81acd9f156edfec87b73fb4071?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hydUx-mpKh7SFVm5UVZ0QYgmo85nNdRVqr8cNaZ35tMtd2f7ylKSv~cZqpoMAHUAF2DHfVLiJbqaGnRgiC6GuALwMTGuwkSofMTy4ArvnphrafvT1-mKCec2ls8Qs18SK1kUQFWnjMkM1~JxOL4V20v5pG3qUrTQZbgXUEKk3K3XgXU~hPv3KdbrTMD5EjhbtsCTIylxUMkvB5HDMQmtSn525f3nA-cex2Vo0txNxskTeKxGepGD6BelfKe5UlzTa1kCheESklus-wicgvmCqoKNrw8Jh4iLY0Vv9pido9EBBYBjPu4riyJ8OkfqUHRufXH-Q-yPQbx-XA5X2hsM2w__',
+		ingredients: ['soda', 'cream', 'milk', 'sugar'],
+		categoryId: 'desserts-001',
+		description:
+			'Refreshing ice cream blended with fizzy soda for a unique twist.'
+	},
+	{
+		id: 'dess-3e2c-54f7-74c6-icecreamhelping',
+		name: 'Ice Cream Helping',
+		price: 12.0,
+		imageSrc:
+			'https://s3-alpha-sig.figma.com/img/27ce/6bfd/83bb0d57e316c33e2a65268484b1241e?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=NacoRUwGy-YIWc1yg6Uend-Rnn1uQtLljc4kx9b9a8noeR11Ft9nvHPh7XTQq1XeFs14I926qLfGIVoH9j6F9oZWNPTey4Y6eIbP-50Yi1NTkNLv0Ii3UFL54lihoPKbTpWB3xtYRgKIxa3-G0XSgG35GSsShJXhqa6Y8AXaMY-d~rIJvYqGPKzE6dLV3xt1jRB7wadEXWD9p-Jkc07LHxzLR795ohOKP5kFfgO6jaKt1c8A2tpiB2xAPTfX7T9D8pgkDiUMtEeCuEzQnG7nf8oWoIF6~7rLs1ni-mt11uXqk5Nt7ieG8QTBklgXBdAZIKdFPzbtG~y81X8USwdLPQ__',
+		ingredients: ['soda', 'cream', 'milk', 'sugar'],
+		categoryId: 'desserts-001',
+		description:
+			'A generous helping of ice cream and soda for the perfect summer treat.'
+	},
+	{
+		id: 'dess-4f5a-7c1d-1b8e-icecreamdelight',
+		name: 'Ice Cream Delight',
+		price: 11.25,
+		imageSrc:
+			'https://s3-alpha-sig.figma.com/img/3c5f/47e2/48dac3f1de8441d08a1d8517f818f0c9?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Vv9hNz3LWzX0cJcuVBrS5AhJGlGnyBjW4IbFZA6QGKx3jyFhcyVDsB-BdaeCuRtyxNilIRolXJZzPAobAdE~jCX1zjcfxQldBtioTfSBX9ShDLX6pex8Nz2e3JEqLZr5g~apsp5E4jdJONc~9oHVGmhRh9a2fUSrBfSTR3KFhh5h5ccFSCE3CW9eKUOjsfoiBL-~lda0gvQcJ~fyGFZidFcpnDrEsirixaJHlgHfuDsF586BQz9wh-oep-d1jLBFfOpRlO06VJTTsR1I5wSxA-CjA3M798goHNXTRoQqHckQN0INfMjPz12VXAsGQ8drBt4gyeQXoOz6aYroHDqrHw__',
+		ingredients: ['soda', 'cream', 'milk', 'sugar'],
+		categoryId: 'desserts-001',
+		description: 'A delightful scoop of ice cream served with sparkling soda.'
+	},
+	{
+		id: 'dess-5c8d-91f2-7d6e-icecreamspecial',
+		name: 'Ice Cream Special',
+		price: 13.75,
+		imageSrc:
+			'https://s3-alpha-sig.figma.com/img/ccbd/f1fa/e2f1c28abfedb4fabf3906f409465d5d?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=UnaS6h95HYNSoSw5rLfnW1Zcc32jjqxt-Tk9Vn74BGrwivEiy04JiUPqBZBcupzmfAA8PUUzUFdJ5eDNhOb391iJMrPnHhmKhMu63iNkDF2w-g9NbEerjXAR9AQ2013cFDMzHQcqxRMN-5r~4Vjk0zg0NIl3Xa8yp6zz0gEtfaWwsQVA-xuGE0vkMyN1mj19jxs68E9AxCAZ~5ywBxSyooEsOKA0z5pL4sAp7Oys9OM3wH5Pfe1lhXIRJAqL5jPRSGI5-CcGjEU89xqb1ZdbLsqxTCbqvp3vb9s5t8OQQWnhnLuzfZ7KMC7H0Ii6rBMiUZQle9N-Gkz9NnBxpJyC0w__',
+		ingredients: ['soda', 'cream', 'milk', 'sugar'],
+		categoryId: 'desserts-001',
+		description:
+			'A special blend of ice cream flavors and fizzy soda for a refreshing experience.'
+	},
+	{
+		id: 'dess-6b4e-25f3-4a9b-icecreamultimate',
+		name: 'Ice Cream Ultimate',
+		price: 14.99,
+		imageSrc:
+			'https://s3-alpha-sig.figma.com/img/a21f/b0b0/57be6e88ae3663273b5151109dc6a44a?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Wb2hzbWqTT6k37ITR2zh3gp-Kytn5RUhYvRNznlq9PMeWaW8Xp-W6rHKprlzZphEu9CZtDTLEy0TVeC3Jz1IH3CZp-NRMXHSk8aTmbIGvC8z0UOeMualRNIAeJRkvjN-wTmaajD77VfGcfunN3qmURkcJVSRRTli5rB81Q3iLCZ1JB7YJW~-0rDs72~jE8UBJdzDPxgqgTuyhCmpa3QeKMRMwI4oh8kg8v0vjlumHy6JZSrJ80tdiE9GhQ42WcAhPTNeEUJGGvgMLU0~bTmnD4xsy85SAmoqpzmFZL16Jb8EzGB1S1ICnP-laC~uW26-A83mh9oy55SJUhTsboGAUg__',
+		ingredients: ['soda', 'cream', 'milk', 'sugar'],
+		categoryId: 'desserts-001',
+		description:
+			'The ultimate ice cream treat with multiple layers of flavor and texture.'
+	},
+	// Hot Drinks
+	{
+		id: 'drinkhot-7a9d-88f6-9b1c-hotchocolate',
+		name: 'Hot Chocolate',
+		price: 4.99,
+		imageSrc:
+			'https://www.foodandwine.com/thmb/V1OEgtLQGUv_w2Fvm40WMLsJ4rk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Indulgent-Hot-Chocolate-FT-RECIPE0223-fd36942ef266417ab40440374fc76a15.jpg',
+		ingredients: ['cocoa', 'milk', 'sugar', 'whipped cream'],
+		categoryId: 'hot-drinks-002',
+		description:
+			'Rich and creamy hot chocolate topped with fluffy whipped cream.'
+	},
+	{
+		id: 'drinkhot-8e7f-5b7a-6a4e-espresso',
+		name: 'Espresso',
+		price: 3.5,
+		imageSrc:
+			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrjyHlkViXf1t9FlpAnmlozT9DZSvPbFi-Lg&s',
+		ingredients: ['coffee beans', 'water'],
+		categoryId: 'hot-drinks-002',
+		description:
+			'A shot of strong and bold espresso, perfect for coffee lovers.'
+	},
+	// Cold Drinks
+	{
+		id: 'drinkcold-9c5e-12f3-7e5a-icedlatte',
+		name: 'Iced Latte',
+		price: 5.25,
+		imageSrc:
+			'https://anerdcooks.com/wp-content/uploads/2022/10/iced_vanilla_latte7.jpg',
+		ingredients: ['espresso', 'milk', 'ice'],
+		categoryId: 'cold-drinks-003',
+		description:
+			'Smooth and refreshing iced latte made with rich espresso and cold milk.'
+	},
+	{
+		id: 'drinkcold-10a2-3f8e-5b7e-lemonade',
+		name: 'Lemonade',
+		price: 3.99,
+		imageSrc:
+			'https://mojo.generalmills.com/api/public/content/S3v1myzakEC_8MtBvag4gw_webp_base.webp?v=7cce746c&t=e724eca7b3c24a8aaa6e089ed9e611fd',
+		ingredients: ['lemons', 'sugar', 'water'],
+		categoryId: 'cold-drinks-003',
+		description:
+			'Freshly squeezed lemonade with just the right amount of sweetness.'
+	},
+	// National Foods
+	{
+		id: 'natfood-11c7-4e3d-9a8f-fruitsalad',
+		name: 'Fruit Salad',
+		price: 8.0,
+		imageSrc:
+			'https://www.hipmamasplace.com/wp-content/uploads/2024/06/summer-fruit-salad-enhanced-1440x1152.jpg',
+		ingredients: ['mixed fruits', 'honey', 'mint'],
+		categoryId: 'national-foods-004',
+		description:
+			'A vibrant mix of seasonal fruits drizzled with honey and garnished with mint.'
+	},
+	{
+		id: 'natfood-12d8-2a5e-3b9a-pastaprimavera',
+		name: 'Pasta Primavera',
+		price: 11.0,
+		imageSrc:
+			'https://dfwblobstorage.blob.core.windows.net/images/Recipe/One-Pot-Shrimp-Pasta-Primavera-wc.jpg',
+		ingredients: ['pasta', 'vegetables', 'olive oil'],
+		categoryId: 'national-foods-004',
+		description:
+			'A delightful pasta dish loaded with fresh vegetables and a light olive oil sauce.'
+	},
+	// Eastern Cuisine
+	{
+		id: 'eastfood-13a9-9f7c-4b8e-falafel',
+		name: 'Falafel',
+		price: 6.5,
+		imageSrc:
+			'https://thenewbaguette.com/wp-content/uploads/2022/09/falafel-patties.jpg',
+		ingredients: ['chickpeas', 'spices', 'herbs'],
+		categoryId: 'eastern-cuisine-005',
+		description:
+			'Crispy falafel patties made from chickpeas and spices, served with tahini.'
+	},
+	{
+		id: 'eastfood-14c3-8d2e-5f6b-shawarma',
+		name: 'Shawarma',
+		price: 9.99,
+		imageSrc: 'https://recipe30.com/wp-content/uploads/2022/07/Shawarma.jpg',
+		ingredients: ['meat', 'pita bread', 'vegetables', 'sauce'],
+		categoryId: 'eastern-cuisine-005',
+		description:
+			'Succulent shawarma wrapped in warm pita bread with fresh vegetables and sauce.'
+	},
+	{
+		id: 'eastfood-15f8-3e7b-2a4d-hummus',
+		name: 'Hummus',
+		price: 5.5,
+		imageSrc:
+			'https://www.allrecipes.com/thmb/Ltz5ZZ15Tx9CnEHjUr54gMswdSA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/232962-super-easy-hummus-ddmfs-1X2-0316-bf44d147efde4432b6f3b4a0c251ac06.jpg',
+		ingredients: ['chickpeas', 'tahini', 'garlic', 'lemon'],
+		categoryId: 'eastern-cuisine-005',
+		description:
+			'Smooth and creamy hummus made from chickpeas, tahini, and lemon, served with pita.'
+	},
+	// Fast Foods
+	{
+		id: 'fastfood-16d2-8a7b-3e1c-burger',
+		name: 'Burger',
+		price: 8.5,
+		imageSrc:
+			'https://www.foodandwine.com/thmb/DI29Houjc_ccAtFKly0BbVsusHc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/crispy-comte-cheesburgers-FT-RECIPE0921-6166c6552b7148e8a8561f7765ddf20b.jpg',
+		ingredients: ['beef patty', 'cheese', 'lettuce', 'tomato'],
+		categoryId: 'fast-foods-006',
+		description:
+			'Juicy beef burger topped with melted cheese, lettuce, and fresh tomato.'
+	},
+	{
+		id: 'fastfood-17e5-4b3e-1c5d-frenchfries',
+		name: 'French Fries',
+		price: 3.5,
+		imageSrc:
+			'https://www.budgetbytes.com/wp-content/uploads/2023/12/air-fryer-french-fries-horizontal-hero-web-ready-1.jpg',
+		ingredients: ['potatoes', 'salt'],
+		categoryId: 'fast-foods-006',
+		description: 'Crispy golden French fries, perfectly salted and served hot.'
+	},
+	{
+		id: 'fastfood-18f4-6a2d-7e8c-chickennuggets',
+		name: 'Chicken Nuggets',
+		price: 5.99,
+		imageSrc:
+			'https://www.allrecipes.com/thmb/DOhcP7hAGP_ams-a-M8A-16TeK4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/AR-161469-the-best-ever-chicken-nuggets-DDMFS-4x3-e0f5af0ce26241d888967904f66962c7.jpg',
+		ingredients: ['chicken', 'bread crumbs', 'spices'],
+		categoryId: 'fast-foods-006',
+		description:
+			'Crispy chicken nuggets, seasoned and breaded to perfection, served with your choice of sauce.'
+	}
+]
