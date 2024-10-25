97a4642890cafc4194ae82bab07d9d74a9ea906f
nurmuhammetjs13 <nurmuhammedaltybaev@gmail.com>
2024-10-25 20:27:24 +0600

    pulled main for Nurchik

--- src/components/pages/Interer/Interer.tsx
+++ src/components/pages/Interer/Interer.tsx
@@ -6,7 +6,6 @@ import IconBannerRight from "@/assets/icons/IconBannerRight";
 import interer1 from "../../../assets/images/1.svg";
 import interer2 from "../../../assets/images/interer2.png";
 
-
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -14,52 +13,55 @@ import "./Interer.css";
 import Image from "next/image";
 
 const Interer = () => {
-  const settings = {
-    dots: false,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 9000,
-    pauseOnHover: true,
-    arrows: false, 
-  };
+    const settings = {
+        dots: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 9000,
+        pauseOnHover: true,
+        arrows: false,
+    };
 
-  return (
-    <div className={styles.mainInterer}>
-      <div className="container">
-        <div className={styles.interer}>
-          <div className={styles.intererText}>
-            <IconBannerLeft />
-            <Typography variant="h2" weight="light" fontFamily="shalimar">
-              Modern Interior
-            </Typography>
-            <IconBannerRight />
-          </div>
-          <div className={styles.slider}>
-            <Slider {...settings}>
-              <div className={styles.mainImg}>
-                <Image src={interer1} alt="img1" />
-              </div>
-              <div className={styles.mainImg}>
-                <Image src={interer2} alt="img1" />
-              
-              </div>
-              <div className={styles.mainImg}>
-                <Image src={interer1} alt="img1" />
-              </div>
-              <div className={styles.mainImg}>
-                <Image src={interer2} alt="img1" />
-              </div>
-              <div className={styles.mainImg}>
-                <Image src={interer1} alt="img1" />
-              </div>
-            </Slider>
-          </div>
+    return (
+        <div className={styles.mainInterer}>
+            <div className="container">
+                <div className={styles.interer}>
+                    <div className={styles.intererText}>
+                        <IconBannerLeft />
+                        <Typography
+                            variant="h2"
+                            weight="light"
+                            fontFamily="shalimar"
+                        >
+                            Modern Interior
+                        </Typography>
+                        <IconBannerRight />
+                    </div>
+                    <div className={styles.slider}>
+                        <Slider {...settings}>
+                            <div className={styles.mainImg}>
+                                <Image src={interer1} alt="img1" />
+                            </div>
+                            <div className={styles.mainImg}>
+                                <Image src={interer2} alt="img1" />
+                            </div>
+                            <div className={styles.mainImg}>
+                                <Image src={interer1} alt="img1" />
+                            </div>
+                            <div className={styles.mainImg}>
+                                <Image src={interer2} alt="img1" />
+                            </div>
+                            <div className={styles.mainImg}>
+                                <Image src={interer1} alt="img1" />
+                            </div>
+                        </Slider>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Interer;
