2fcb3f7e2b3b1e26bb51c60cdd0d9e70fc8db886
shaiyldaevich <salamalikbaryktabasov@gmail.com>
2024-10-28 11:53:44 +0600

    configure

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['s3-alpha-sig.figma.com'],
-      },
+  images: {
+    domains: [
+      's3-alpha-sig.figma.com',
+      'www.foodandwine.com',
+      'encrypted-tbn0.gstatic.com',
+      'anerdcooks.com',
+      'www.hipmamasplace.com',
+      'mojo.generalmills.com',
+      'dfwblobstorage.blob.core.windows.net',
+      'thenewbaguette.com',
+      'recipe30.com',
+      'www.allrecipes.com',
+      'www.budgetbytes.com'  // Добавлен домен budgetbytes.com
+    ],
+  },
 };
 
 export default nextConfig;
